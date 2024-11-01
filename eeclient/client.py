6bd630f465b136bd033cebe02b51c8a7455340fd
Daniel Guerrero <dfgm2006@gmail.com>
2024-11-01 14:49:51 +0100

    feat: introduce async ee client (#4)
    * feat: introduce async ee client
    
    * rest
    
    * session

--- eeclient/client.py
+++ eeclient/client.py
@@ -1,25 +1,117 @@
-from typing import Literal
+import json
+from pathlib import Path
+import time
+from typing import Literal, Union
 import httpx
+from ee.oauth import CLIENT_ID, CLIENT_SECRET, SCOPES, get_credentials_path
+from eeclient.aioclient import PathLike
 from eeclient.exceptions import EERestException
+from eeclient.typing import GoogleTokens, SepalHeaders
+
+HEADERS = json.loads((Path(__file__).parent / "config.json").read_text())
 
 
 class Session:
-    def __init__(self, credentials, ee_project):
+    def __init__(
+        self,
+        ee_project: str,
+        credentials: Union[PathLike, dict],
+        sepal_headers: Union[SepalHeaders, None] = None,
+    ):
+        """Session that handles two scenarios to set the headers for the Earth Engine API
 
-        self.credentials = credentials
+        It can be initialized with the headers sent by SEPAL or with the credentials and project
+
+        Args:
+            ee_project (str): The project id of the Earth Engine project
+            credentials (Union[PathLike, dict]): The credentials to use for the session
+            sepal_headers (Union[SepalHeaders, None], optional): The headers sent by SEPAL. Defaults to None.
+        """
+        self.headers = None
         self.project = ee_project
+        self.credentials = None
 
-        self.headers = {
-            "x-goog-user-project": ee_project,
-            "Authorization": f"Bearer {self._get_access_token()}",
-        }
+        # If initializing from credentials and project, load credentials
+        if not sepal_headers:
+            if not ee_project or not credentials:
+                raise ValueError(
+                    "Both ee_project and credentials must be provided when from_headers is False."
+                )
 
-        self.client = httpx.Client(headers=self.headers)
+            if isinstance(credentials, (str, Path)) or not credentials:
+                credentials_path = credentials or get_credentials_path()
+                credentials = json.loads(Path(credentials_path).read_text())
+
+            self.credentials = credentials
+            self.headers = {
+                "x-goog-user-project": ee_project,
+                "Authorization": f"Bearer {self._get_access_token()}",
+            }
+
+        else:
+            self.sepal_headers = sepal_headers
+            self.headers = self.get_headers()
+
+    @headers.setter
+    def headers(self, headers):
+        self.headers = headers
+
+    @headers.getter
+    def headers(self):
+
+        if self.from_sepal:
+            return self.get_headers()
+
+        return self.headers
 
     def _set_url_project(self, url):
         """Set the project in the url"""
 
         return url.format(project=self.project)
+
+    @property
+    def is_expired(expiry_date: int) -> bool:
+        """Returns if a token is about to expire"""
+
+        return expiry_date - time.time() < 60
+
+    def get_headers(self):
+        """Set the headers from SEPAL"""
+
+        if self.sepal_headers:
+
+            google_tokens: GoogleTokens = self.sepal_headers.get("googleTokens")
+
+            username = google_tokens.get("username")
+            expiry_date = google_tokens.get("accessTokenExpiryDate")
+            project_id = google_tokens.get("projectId")
+
+            if not self.is_expired(expiry_date):
+
+                access_token = google_tokens.get("accessToken")
+
+            else:
+                self.sepal_headers = self.refresh_sepal_google_tokens(username)
+                return self.get_headers()
+
+            return {
+                "x-goog-user-project": project_id,
+                "Authorization": f"Bearer {access_token}",
+            }
+
+        return self.headers
+
+    def get_sepal_google_tokens(self, username: str) -> GoogleTokens:
+        """"""
+
+        credentials_endpoint = "/api/users/{username}/credentials"
+        url = f"https://sepal.io{credentials_endpoint}"
+
+        with httpx.Client(headers=self.sepal_headers) as client:
+
+            response = client.get(url)
+
+        return response.json()
 
     def rest_call(
         self,
@@ -48,7 +140,7 @@ class Session:
         return response.json()
 
     def _get_access_token(self):
-        """Get the access token from the refresh token"""
+        """Get the access token from the refresh token using the credentials file"""
 
         url = "https://oauth2.googleapis.com/token"
 
