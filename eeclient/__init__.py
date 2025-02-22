3f2d1de9ff63c9a8a032370056bb27371c8fde83
dfguerrerom <dfgm2006@gmail.com>
2025-02-21 19:19:37 -0500

    bump: version 1.1.0 → 1.1.1

--- eeclient/__init__.py
+++ eeclient/__init__.py
@@ -1,6 +1,6 @@
 __title__ = "eeclient"
 __summary__ = "A client for Google Earth Engine"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 __author__ = "Daniel Guerrero"
 __email__ = "dfgm2006@gmail.com"
